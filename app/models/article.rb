ecf418d7c97049993e49f0ec769470d9d0a54175
kimu805 <kimukimuok805@gmail.com>
2026-02-02 16:37:23 +0900

    repair

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,11 @@
 class Article < ApplicationRecord
   enum :status, { draft: 0, published: 1, archived: 2 }, default: :draft, prefix: true
 
-  validates :title,  presence: { message: 'を入力してください' },
-                     length:   { maximum: 255, message: 'は255文字以内で入力してください' }
-  validates :body,   presence: { message: 'を入力してください' },
-                     length:   { maximum: 100_000, message: 'は100000文字以内で入力してください' }
-  validates :status, presence: { message: 'を入力してください' }
+  validates :title,  presence: { message: "を入力してください" },
+                     length:   { maximum: 255, message: "は255文字以内で入力してください" }
+  validates :body,   presence: { message: "を入力してください" },
+                     length:   { maximum: 100_000, message: "は100000文字以内で入力してください" }
+  validates :status, presence: { message: "を入力してください" }
 
   validate :validate_published_at_presence
 
@@ -16,7 +16,7 @@ class Article < ApplicationRecord
 
   def validate_published_at_presence
     if status_published? && published_at.blank?
-      errors.add(:published_at, 'を入力してください')
+      errors.add(:published_at, "を入力してください")
     end
   end
 
